4257014d5e505759424cafe9cc6c475f43613d32
simhyunbo <hbo9347@naver.com>
2022-10-05 09:18:21 +0900

    1005 인텔리

---
+++ SwapExercise.java
@@ -0,0 +1,12 @@
+public class SwapExercise {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.printf("a:%d b:%d", a, b);
+    }
+}
